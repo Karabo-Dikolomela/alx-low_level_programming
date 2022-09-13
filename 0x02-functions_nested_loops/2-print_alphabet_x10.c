3ce724f1c8537c39e931b82ed03b18879e0f4268
Karabo-Dikolomela <karabodikolomela2@gmail.com>
2022-09-13 16:55:17 -0700

    created 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int you, me;
+
+	you = 0;
+
+	while (you <= 9)
+	{
+		for (me = 97; me <= 122; me++)
+		{
+			_pitchar(me);
+		}
+		_putchar('\n');
+	}
+}
+
