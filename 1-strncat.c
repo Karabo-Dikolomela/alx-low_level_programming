8635cce8bd2aa1f076d3d980f66b8f704f371e54
Karabo-Dikolomela <karabodikolomela2@gmail.com>
2022-09-21 12:24:35 -0700

    created 1-strncat.c file

---
+++ 1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+ /**
+  * _strncat - concatenates two strings,
+  * @dest: destination.
+  * @src: source.
+  * @n: amount of bytes used from src.
+  * Return: the pointer to dest.
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0, y = 0;
+
+	while (*(dest + x) != '\0')
+	{
+		x++;
+	}
+
+	while (y < n)
+	{
+		*(dest + x) = *(src + y);
+		if (*(src + y) == '\0')
+			break;
+		x++;
+		y++;
+	}
+	return (dest);
+}
