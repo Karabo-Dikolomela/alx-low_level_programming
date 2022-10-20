5c7df7d74eae9cc7a508caae0903f7f3ee748f4e
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-10-20 22:42:26 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,9 +10,10 @@ size_t list_len(const list_t *h)
 	size_t nelem;
 
 	nelem = 0
-		while (h != NULL)
-		{ h = h->next;
-			nelem++;
-		}
+	while (h != NULL)
+	{ 
+		h = h->next;
+		nelem++;
+	}
 	return (nelem);
 }
