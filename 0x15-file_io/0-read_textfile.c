1baeb9b5f07c4f3c3eb8c7d9f0edaf461675dd27
Karabo-Dikolomela <karabodikolomela2@gmail.com>
2022-11-03 10:44:27 -0700

    files added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed.It fails, returns 0.
+ */
