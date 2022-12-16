64746a47953e29451c69c8dadf107d0f9d40b312
Karabo-Dikolomela <karabodikolomela2@gmail.com>
2022-12-16 12:40:27 -0800

    Update

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs -Computes the absolute value
+ * of an integer.
+ *
+ * @k: input number as an integer.
+ *
+ * Return: absolute value
+ */
+int _abs(int k)
+{
+	if (k >= 0)
+	{
+		return (k);
+	}
+	else
+	{
+		return (k * -1);
+	}
+}
